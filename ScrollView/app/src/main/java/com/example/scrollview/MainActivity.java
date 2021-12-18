916997571b0bd204863125c08e173ffd862f825a
Erli <85597743+Erli-Erli@users.noreply.github.com>
2021-12-18 17:43:08 +0700

    done

---
+++ ScrollView/app/src/main/java/com/example/scrollview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.scrollview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.purple_500)));
+    }
+}
